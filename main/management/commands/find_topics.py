ddec407226588fc2530d0089792c0246ac648ab2
Zsolt <zsolt@sess.hu>
2023-05-02 21:09:08 +0200

    Making the topic categorization algorythm more efficient.

--- main/management/commands/find_topics.py
+++ main/management/commands/find_topics.py
@@ -5,10 +5,19 @@ class Command(BaseCommand):
     help = 'Find topics for all articles'
 
     def handle(self, *args, **options):
-        articles = Article.objects.all()
-        for article in articles:
-            topics = Topic.objects.all()
-            for topic in topics:
-                if topic.name.lower() in article.title.lower() or topic.name.lower() in article.description.lower():
-                    article.topics.add(topic)
-        self.stdout.write(self.style.SUCCESS('Successfully found topics for all articles'))
+        # Iterating through all topics
+        for topic in Topic.objects.all():
+            # Finding articles that have the topic title in the title or description.
+            articles = Article.objects.filter(title__icontains=topic.name) | Article.objects.filter(description__icontains=topic.name)
+            # Iterating through all articles that have the topic title in the title or description.
+            for article in articles:
+                # Adding the topic to the article.
+                article.topics.add(topic)
+                # Saving the article.
+                article.save()
+                # Printing the article title and topic name.
+                print(article.title, topic.name)
+                
+            
+            
+        
